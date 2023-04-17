51c0ca12257f8fb9be97031f6d5024e32fc32d38
laney522 <pm72222@gmail.com>
2023-04-17 22:43:12 +0900

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,6 @@ import { Link } from 'gatsby'
 const IndexPage: FunctionComponent = function () {
   return (
     <div>
-      <Text text="Hello, World!" />
       <Link to="/info/">To Info</Link>
     </div>
   )
