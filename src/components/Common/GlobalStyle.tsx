21bd7265fd9ec6d77cedec7649bc957a05b88f18
laney522 <pm72222@gmail.com>
2023-04-25 00:03:35 +0900

    update GlobalStyle.tsx

--- src/components/Common/GlobalStyle.tsx
+++ src/components/Common/GlobalStyle.tsx
@@ -3,5 +3,18 @@ import { Global, css } from '@emotion/react'
 
 const defaultStyle = css`
   @import url('https://fonts.googleapis.com/css2?family=Nanum+Myeongjo:wght@400;700;800&display=swap');
+
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: 'Nanum Myeongjo', serif;
+  }
+
+  html,
+  body,
+  #___gatsby {
+    height: 100%;
+  }
   
 `
