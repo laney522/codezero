0a5c7893fe57c16c32138e61ef2ce4ea2bdf2852
laney522 <pm72222@gmail.com>
2023-04-28 21:43:00 +0900

    update

--- src/components/Main/ProfileImage.tsx
+++ src/components/Main/ProfileImage.tsx
@@ -1,1 +1,19 @@
-  
+import React, { FunctionComponent } from 'react'
+import styled from '@emotion/styled'
+
+// person settings
+const PROFILE_IMAGE_LINK = 
+  '<https://avatars.githubusercontent.com/u/62184014?s=400&v=4>'
+
+const ProfileImageWrapper = styled.img`
+  width: 120px;
+  height: 120px;
+  margin-bottom: 30px;
+  border-radius: 50px;
+`
+
+const ProfileImage: FunctionComponent = function () {
+  return <ProfileImageWrapper src={PROFILE_IMAGE_LINK} alt="Profile Image" />
+}
+
+export default ProfileImage
