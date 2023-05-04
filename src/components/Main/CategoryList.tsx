9d1c795c0a55972bdfc351118fd587f112c61bc5
laney522 <pm72222@gmail.com>
2023-05-04 21:55:48 +0900

    update CategoryList.tsx

--- src/components/Main/CategoryList.tsx
+++ src/components/Main/CategoryList.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
+import { Link } from 'gatsby'
 
 export type CategoryListProps = {
   selectedCategory: string
@@ -14,6 +15,16 @@ const CategoryListWrapper = styled.div`
   flex-wrap: wrap;
   width: 768px;
   margin: 100px auto 0;
+`
+const CategoryItem = styled(Link)`
+  margin-right: 20px;
+  padding: 5px 0;
+  font-size: 18px;
+  cursor: pointer;
+
+  &:last-of-type {
+    margin-right: 0;
+  }
 `
 
 const CategoryList: FunctionComponent<CategoryListProps> = function ({
