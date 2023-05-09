e19820af397e66ec62f3edc4f7611cb471114e78
laney522 <pm72222@gmail.com>
2023-05-09 21:17:39 +0900

    update PostList.tsx

--- src/components/Main/PostList.tsx
+++ src/components/Main/PostList.tsx
@@ -1,6 +1,16 @@
 import React, { FunctionComponent } from 'react';
 import styled from '@emotion/styled';
 
+const POST_ITEM_DATA = {
+  title: 'Post Item Title',
+  date: '2020.01.29',
+  categories: ['Web', 'Frontend', 'Testing'],
+  summary:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident repellat doloremque fugits rem temporibus! Maxime molestias, sumtrem debitis odit harum impedit. Modi cupiditate harum dignissimos eos in corrupti!',
+  thumbnail:
+    '<https://ji5485.github.io/static/>'
+}
+
 const PostListWrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
