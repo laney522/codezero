5ea79bdc20166a417a90b706c650f5b4579496c3
laney522 <pm72222@gmail.com>
2023-05-12 22:17:53 +0900

    update PostItem.tsx

--- src/components/Main/PostItem.tsx
+++ src/components/Main/PostItem.tsx
@@ -1,4 +1,6 @@
 import React, { FunctionComponent } from 'react'
+import styled from '@emotion/styled'
+import { Link } from 'gatsby'
 
 type PostItemProps = {
   title: string
@@ -8,6 +10,17 @@ type PostItemProps = {
   thumbnail: string
   link: string
 }
+
+const PostItemWrapper = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  border-radius: 10px;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.15);
+  transition: 0.3s box-shadow;
+  cursor: pointer;
+
+  
+`
 
 const PostItem: FunctionComponent<PostItemProps> = function ({
   title,
