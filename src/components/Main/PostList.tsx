b35846faf6a5e326b0b419f39d509e6d7541af6d
laney522 <pm72222@gmail.com>
2023-05-16 23:18:35 +0900

    update

--- src/components/Main/PostList.tsx
+++ src/components/Main/PostList.tsx
@@ -9,9 +9,13 @@ const POST_ITEM_DATA = {
   summary:
     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident repellat doloremque fugits rem temporibus! Maxime molestias, sumtrem debitis odit harum impedit. Modi cupiditate harum dignissimos eos in corrupti!',
   thumbnail:
-    '<https://ji5485.github.io/static/e4f34c558ae8e8235ff53b0311085796/4d854/javascript-core-concept-summary-function-1.webp>',
+    //'<https://e7.pngegg.com/pngimages/788/87/png-clipart-tropical-plants-green-leaves-tropical-plant-thumbnail.png>',
+    //'<https://ji5485.github.io/static/e4f34c558ae8e8235ff53b0311085796/4d854/javascript-core-concept-summary-function-1.webp>',
+    // '<https://lh3.googleusercontent.com/BCLrZSQ149dscBBnChfnOAFTzmT-T8EyV5Z4s2qRy2Mg_ySFwnnWIu_zZrO8XdsDMKBfdwSEU1YCCLCQbFyMBerfnuJYoeSGUyy6cGkDLw>',
+    '..\images\pngegg.png',
   link: '<https://www.google.co.kr/>',
 }
+
 
 const PostListWrapper = styled.div`
   display: grid;
@@ -24,12 +28,12 @@ const PostListWrapper = styled.div`
 
 const PostList: FunctionComponent = function () {
   return (
-  <PostListWrapper>
-    <PostItem {...POST_ITEM_DATA} />
-    <PostItem {...POST_ITEM_DATA} />
-    <PostItem {...POST_ITEM_DATA} />
-    <PostItem {...POST_ITEM_DATA} />
-  </PostListWrapper>
+    <PostListWrapper>
+      <PostItem {...POST_ITEM_DATA} />
+      <PostItem {...POST_ITEM_DATA} />
+      <PostItem {...POST_ITEM_DATA} />
+      <PostItem {...POST_ITEM_DATA} />
+    </PostListWrapper>
   )
 }
 
