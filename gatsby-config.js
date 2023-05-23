c3894f36fe4e59c4e39c31a1edf3e4ad81d2c358
laney522 <pm72222@gmail.com>
2023-05-23 19:34:36 +0900

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,27 @@
 
-module.exports = { 
-  siteMetadata: { 
-    title: `Gatsby Default Starter`, 
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs` 
-  }, 
-  plugins: [ 
-    { 
-      resolve: 'gatsby-plugin-typescript', 
-      options: { 
-        isTSX: true, 
-        allExtentions: true, 
-      }, 
-    }, 
-    `gatsby-plugin-react-helmet`, 
-    `gatsby-transformer-sharp`, 
-    `gatsby-plugin-sharp`, 
-  ], 
+    author: `@gatsbyjs`,
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-typescript',
+      options: {
+        isTSX: true,
+        allExtentions: true,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `contents`,
+        path: `${__dirname}/contents`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+  ],
 }; 
