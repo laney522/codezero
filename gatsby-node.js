3e0f7707461190dc54f5df2e2d91ed73ecac884c
laney522 <pm72222@gmail.com>
2023-06-04 22:49:43 +0900

    update

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,14 +5,14 @@ const path = require('path');
 exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
   const output = getConfig().output || {};
 
-  actions.setWebpackConfig({
-    output,
-    resolve: {
-      alias: {
-        components: path.resolve(__dirname, 'src/compoments'),
-        utils: path.resolve(__dirname, 'src/utils'),
-        hooks: path.resolve(__dirname, 'src/hooks'),
-      },
-    },
-  });
-};
+  actions.setWebpackConfig({  
+    output,  
+    resolve: {  
+      alias: {  
+        components: path.resolve(__dirname, 'src/compoments'),  
+        utils: path.resolve(__dirname, 'src/utils'),  
+        hooks: path.resolve(__dirname, 'src/hooks'),  
+      },  
+    }, 
+  }); 
+}; 
