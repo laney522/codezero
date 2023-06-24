ee3a03e9cc640564c061ec041fdc77ed827a73fb
laney522 <pm72222@gmail.com>
2023-06-24 23:04:21 +0900

    update

--- src/components/Main/CategoryList.tsx
+++ src/components/Main/CategoryList.tsx
@@ -14,17 +14,17 @@ type GatsbyLinkProps = {
 
 export type CategoryListProps = {
   selectedCategory: string
-  categoryList: {
+  categoryList: { 
     // 프로퍼티 이름은 문자열, 프로퍼티 값은 숫자입을 나타내는 타입 표기 방법
     [key: string]: number
   }
 }
 
 const CategoryListWrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  width: 768px;
-  margin: 100px auto 0;
+  display: flex; 
+  flex-wrap: wrap; 
+  width: 768px; 
+  margin: 100px auto 0; 
 `
 
 // eslint-disable-next-line @typesript-eslint/no-unused-vars
