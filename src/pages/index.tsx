0e434377c76cf0ee431721e2b1984e73a728b341
laney522 <pm72222@gmail.com>
2023-07-05 22:04:23 +0900

    update IndexPage function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,12 @@ import Footer from '../components/Common/Footer'
 import CategoryList from '../components/Main/CategoryList'
 import Introduction from '../components/Main/Introduction'
 import PostList from '../components/Main/PostList'
+import { graphql } from 'gatsby'
 
 const CATEGORY_LIST = {
-  All: 5, 
-  Web: 3, 
-  Mobile: 2, 
+  All: 5,
+  Web: 3,
+  Mobile: 2,
 }
 
 const Container = styled.div` 
@@ -18,16 +19,44 @@ const Container = styled.div`
   height: 100%;   
 `
 
-const IndexPage: FunctionComponent = function () { 
-  return ( 
-    <Container> 
-      <GlobalStyle /> 
-      <Introduction /> 
-      <CategoryList selectedCategory='Web' categoryList={CATEGORY_LIST} /> 
-      <PostList /> 
-      <Footer /> 
-    </Container> 
+const IndexPage: FunctionComponent = function ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) {
+  return (
+    <Container>
+      <GlobalStyle />
+      <Introduction />
+      <CategoryList selectedCategory='Web' categoryList={CATEGORY_LIST} />
+      <PostList />
+      <Footer />
+    </Container>
   )
 }
 
+
 export default IndexPage
+
+export const getPostList = graphql`
+  query getPostList {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date, frontmatter___title] }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            summary
+            date(formatString: "YYYY.MM.DD.")
+            categories
+            thumbnail {
+              publicURL
+            }
+          }
+        }
+      }
+    }
+  }
+`
